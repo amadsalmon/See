e0585f9f5449c4506ab01de0ce36d2372923fa92
Amad Salmon <amad.salmon@icloud.com>
2020-08-27 14:22:02 +0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Amad Salmon on 27/08/2020.
+//  Copyright © 2020 Amad Salmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
